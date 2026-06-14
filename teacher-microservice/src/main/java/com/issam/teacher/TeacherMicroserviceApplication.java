4fa28e06185493d8c50300a3088a5a2cbea8ec00
Issam El <issamatlati105@gmail.com>
2026-06-14 20:27:17 +0200

    teacher microservice

---
+++ teacher-microservice/src/main/java/com/issam/teacher/TeacherMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.issam.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherMicroserviceApplication.class, args);
+	}
+
+}
