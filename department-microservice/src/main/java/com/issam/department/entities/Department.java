87d292dc652df258571acfd7a1e95501bd2982fe
Issam El <issamatlati105@gmail.com>
2026-06-14 21:01:11 +0200

    Creating Dept entity and DeptDto

---
+++ department-microservice/src/main/java/com/issam/department/entities/Department.java
@@ -0,0 +1,23 @@
+package com.issam.department.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String depName;
+    private String depCode;
+}
