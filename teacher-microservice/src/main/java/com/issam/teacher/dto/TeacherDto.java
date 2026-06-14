ec0ef636a74600bdbd57a8dfe9eaa12702c089e2
Issam El <issamatlati105@gmail.com>
2026-06-14 21:36:47 +0200

    creating teacherDto and teacher entity

---
+++ teacher-microservice/src/main/java/com/issam/teacher/dto/TeacherDto.java
@@ -0,0 +1,15 @@
+package com.issam.teacher.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeacherDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
